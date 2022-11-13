415bb495ef70954bb99f080a469a55bac456c3ed
TheFlyingRat <joeyspp50@gmail.com>
2022-11-13 12:28:59 +1100

    modified:   index.py 	new file:   mii_channel.wav

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 
 
 class Morse:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
 
         self.LOOKUP_TABLE = {   
                 'A': '.-',     'B' : '-...',
@@ -20,12 +20,26 @@ class Morse:
                 '?': '..--..', '/' : '-..-.',  '-': '-....-',
                 '(': '-.--.',  ')' : '-.--.-', ' ': '/',
                 '!': '-.-.--', ';' : '-.-.-.', ':': '---...',
-                '+': '.=.=.',  '=' : '-...-'
+                '+': '.=.=.',  '=' : '-...-',  '' : ''
             }
 
         self.INVERSE_LOOKUP = dict(zip(self.LOOKUP_TABLE.values(), self.LOOKUP_TABLE.keys()))  
 
-    def encode(self, value: str):
+    def encode(self, value: str, *args, **kwargs) -> str:
+        """
+        Encode
+        ------
+
+
+        - Returns a translation of the string given in morse code.
+
+
+
+        :PARAM value: Translatable value
+        
+        :RETURN response: Encoded value
+
+        """
         resp = ""
         for i in value:
             if i.upper() not in self.LOOKUP_TABLE:
@@ -34,14 +48,25 @@ class Morse:
             resp += self.LOOKUP_TABLE[i.upper()] + " "
         return resp
     
-    def decode(self, value: str):
+    def decode(self, value: str, *args, **kwargs) -> str:
+        """
+        Decode
+        ------
+
+
+        - Returns a string representation of the morse code value parameter
+
+
+
+        :PARAM value: Morse code value
+
+        :RETURN response: Decoded value
+
+        """
         resp = ""
-        
         characters = value.split(" ")
         for character in characters:
-            
             resp += self.INVERSE_LOOKUP[character]
-
         return resp
 
 
@@ -50,5 +75,6 @@ morse = Morse()
 
 encoded = morse.encode("Hello world!")
 decoded = morse.decode(encoded)
+morse.decode()
 print(decoded)
 
